c3c2be857aef81d2cfc84542fa8c6b592acc4672
davidluong18 <31758412+davidluong18@users.noreply.github.com>
2019-04-22 23:57:22 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		LexicalAnalyzer lex = new LexicalAnalyzer();
+		lex.run();
+		//lex.close();
+	}
+
+}
